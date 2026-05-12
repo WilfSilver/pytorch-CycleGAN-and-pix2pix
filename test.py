8061a031c20b91d0eaa286106a0a5e01a574a167
Wilf Silver <git@wilfsilver.co.uk>
2026-05-12 12:45:31 +0100

    Add FID evaluation as a quantitative metric

--- test.py
+++ test.py
@@ -32,6 +32,8 @@ from pathlib import Path
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
+from models.fid import calculate_fid
+from util.util import tensor2im
 from util.visualizer import save_images
 from util import html
 import torch
@@ -50,7 +52,7 @@ if __name__ == "__main__":
     opt.batch_size = 1  # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True  # no flip; comment this line if results on flipped images are needed.
-    
+
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)  # create a model given opt.model and other options
     model.setup(opt)  # regular setup: load and print networks; create schedulers
@@ -61,11 +63,16 @@ if __name__ == "__main__":
         web_dir = Path(f"{web_dir}_iter{opt.load_iter}")
     print(f"creating web directory {web_dir}")
     webpage = html.HTML(web_dir, f"Experiment = {opt.name}, Phase = {opt.phase}, Epoch = {opt.epoch}")
+
     # test with eval mode. This only affects layers like batchnorm and dropout.
     # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
     # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
     if opt.eval:
+        print("Running in eval mode")
         model.eval()
+
+    real_imgs = []
+    fake_imgs = []
     for i, data in enumerate(dataset):
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
@@ -76,4 +83,10 @@ if __name__ == "__main__":
         if i % 5 == 0:  # save images to an HTML file
             print(f"processing ({i:04d})-th image... {img_path}")
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+
+        real_imgs.append(tensor2im(next(filter(lambda d: d[0] == "real_B", visuals.items()))[1]))
+        fake_imgs.append(tensor2im(next(filter(lambda d: d[0] == "fake_B", visuals.items()))[1]))
+
     webpage.save()  # save the HTML
+
+    print(calculate_fid([real_imgs, fake_imgs], 50, opt.device, 2048))
